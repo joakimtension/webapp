e2a95b5c250c2d50030b202a0e28534682ed59ed
Joakim Andersson <joakim@tension.se>
2017-02-22 10:56:35 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask("webapp")
+
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+@app.route("/info/<username>")
+def info(username):
+	return render_template("info.html", username=username)
+
+if __name__ == "__main__":
+	app.run()
